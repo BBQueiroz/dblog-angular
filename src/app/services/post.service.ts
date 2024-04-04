e7672fdf0e905ea47dad6707ccc22a327c10a95a
BBQueiroz <bruninhoqueiroz@gmail.com>
2024-04-04 20:36:35 -0300

    Update post.service.ts

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -57,11 +57,10 @@ export class PostService {
 
   changeLike(id: string){
     const headers = this.buildHeadersWithToken();
-    const body = { id };
-    const req = this.http.post<Posts>(`https://dblog-backend.onrender.com/posts/${id}/like`, body, { headers });
+    const req = this.http.post<Posts>(`https://dblog-backend.onrender.com/posts/${id}/like`, { headers });
     req.subscribe({
       next: data => console.log('post publicado', data.title),
-      error: err => console.log('Erro ao publicar', err, body)
+      error: err => console.log('Erro ao publicar', err)
     });
   }
 
